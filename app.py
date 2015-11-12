c7c005add6371f9fe517969c21e6447dad59bb78
bchav <bmchavis@gmail.com>
2015-11-12 20:09:05 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, send_from_directory, render_template
+import os
+import socket
+
+app = Flask(__name__)
+hostname = socket.gethostname()
+
+if "DEBUG" in os.environ:
+    app.debug = True
+
+@app.errorhandler(500)
+def error(e):
+    return render_template('error.html', hostname=hostname, error=e), 500
+
+@app.route("/")
+def index():
+    return render_template("index.html", hostname=hostname)
+
+@app.route("/assets/<path:path>")
+def assets(path):
+    return send_from_directory("assets", path)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
